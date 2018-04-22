7b9967b1cf7b55cb7660c7f2ffee942cd94954e3
Alex Pagnotta <alexpagnottacs@gmail.com>
2018-04-22 17:39:00 +0200

    Add user_id to notes

--- database/migrations/2018_04_22_132403_create_notes_table.php
+++ database/migrations/2018_04_22_132403_create_notes_table.php
@@ -16,7 +16,8 @@ class CreateNotesTable extends Migration
         Schema::create('notes', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title');    
-            $table->string('text');                    
+            $table->string('text');    
+            $table->integer('user_id')->index();                
             $table->timestamps();
         });
     }
